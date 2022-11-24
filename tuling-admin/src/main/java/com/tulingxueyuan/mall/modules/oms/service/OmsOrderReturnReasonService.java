710bb07f226e9925823c7e2d0116afd24bbb1457
luoban <443080804@qq.com>
2022-11-24 18:24:38 +0800

    feat: 退货原因设置

--- tuling-admin/src/main/java/com/tulingxueyuan/mall/modules/oms/service/OmsOrderReturnReasonService.java
+++ tuling-admin/src/main/java/com/tulingxueyuan/mall/modules/oms/service/OmsOrderReturnReasonService.java
@@ -1,7 +1,12 @@
 package com.tulingxueyuan.mall.modules.oms.service;
 
-import com.tulingxueyuan.mall.modules.oms.model.OmsOrderReturnReason;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.tulingxueyuan.mall.dto.OrderReturnReasonAddConditionDTO;
+import com.tulingxueyuan.mall.dto.OrderReturnReasonConditionDTO;
+import com.tulingxueyuan.mall.modules.oms.model.OmsOrderReturnReason;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +18,11 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface OmsOrderReturnReasonService extends IService<OmsOrderReturnReason> {
 
+    Page list(OrderReturnReasonConditionDTO condition);
+
+    boolean add(OrderReturnReasonAddConditionDTO condition);
+
+    boolean updateStatus(int status, Long ids);
+
+    boolean delete(List<Long> ids);
 }
